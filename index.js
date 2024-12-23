1a55c1c7bd591c87ce521ad21ade291852ddfc7a
Ana Júlia Medeiros <julinhamedeiros2013@gmail.com>
2024-12-23 00:11:52 -0300

    Ajustes

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-import express from "express";
-import userRoutes from "./routes/users.js";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from "express"
+import userRoutes from "./routes/users.js"
+import cors from "cors"
 
-dotenv.config();
-
-const app = express();
+const app = express()
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/", userRoutes);
 
-const PORT = process.env.PORT || 8800;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(8800);
